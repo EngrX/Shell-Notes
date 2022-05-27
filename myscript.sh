5939408bf2afd91daa6ba4c22d87ac0e695f7ae7
XIAO <xiaoheroine@gmail.com>
2022-05-27 09:43:04 +0800

    Added noted for Conditional Statements

--- myscript.sh
+++ myscript.sh
@@ -50,17 +50,47 @@
 
 
 ### example of ELSE-IF (ELIF) CONDITION
-read -p "Enter valid name: " NAME
+# read -p "Enter valid name: " NAME
 
-if [ "$NAME" == "JOHN" ]
-then   
-    echo "You're allowed"
-elif [ "$NAME" == "DARLA" ]
+# if [ "$NAME" == "JOHN" ]
+# then   
+#     echo "You're allowed"
+# elif [ "$NAME" == "DARLA" ]
+# then
+#     echo "You're exempted to work"
+# elif [ "$NAME" == "NYMERIA" ]
+# then 
+#     echo "You're cute enought, no need to work"
+# else 
+#     echo "NOT ALLOWED!"
+# fi
+
+
+########
+# COMPARISON  
+# val1 -eq val2 Returns true if the values are equal                  | ==
+# val1 -ne val2 Returns true if the values are not equal              | !=
+# val1 -gt val2 Returns true if val1 is greater than val2             |  >
+# val1 -ge val2 Returns true if val1 is greater than or equal to val2 | >=
+# val1 -lt val2 Returns true if val1 is less than val2                |  <
+# val1 -le val2 Returns true if val1 is less than or equal to val2    | <=
+########
+
+# example using comparison
+# testing area of a rectangle with 12x16 length and width
+read -p "Enter the length of the rectangle: " LENGTH
+read -p "Enter the width of the rectangle: " WIDTH
+AREA=$(($LENGTH*$WIDTH)) #remember when computing | $((expression)) this is required
+
+if [ "$AREA" -eq 192 ]
 then
-    echo "You're exempted to work"
-elif [ "$NAME" == "NYMERIA" ]
+    echo "Area is equal to 192."
+elif [ "$AREA" -gt 192 ]
 then 
-    echo "You're cute enought, no need to work"
-else 
-    echo "NOT ALLOWED!"
+    echo "Area is greater than 192."
+elif [ "$AREA" -lt 192 ]
+then
+    echo "Area is less than 192."
+else
+    echo "Enter length and width"
 fi
