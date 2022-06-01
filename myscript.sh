2ae0c1561ccc8f28c8e4187b859e2ad809781351
XIAO <xiaoheroine@gmail.com>
2022-06-01 12:59:34 +0800

    Finished conditional statements

--- myscript.sh
+++ myscript.sh
@@ -78,19 +78,73 @@
 
 # example using comparison
 # testing area of a rectangle with 12x16 length and width
-read -p "Enter the length of the rectangle: " LENGTH
-read -p "Enter the width of the rectangle: " WIDTH
-AREA=$(($LENGTH*$WIDTH)) #remember when computing | $((expression)) this is required
+# read -p "Enter the length of the rectangle: " LENGTH
+# read -p "Enter the width of the rectangle: " WIDTH
+# AREA=$(($LENGTH*$WIDTH)) #remember when computing | $((expression)) this is required
 
-if [ "$AREA" -eq 192 ]
-then
-    echo "Area is equal to 192."
-elif [ "$AREA" -gt 192 ]
-then 
-    echo "Area is greater than 192."
-elif [ "$AREA" -lt 192 ]
-then
-    echo "Area is less than 192."
-else
-    echo "Enter length and width"
-fi
+# if [ "$AREA" -eq 192 ]
+# then
+#     echo "Area is equal to 192."
+# elif [ "$AREA" -gt 192 ]
+# then 
+#     echo "Area is greater than 192."
+# elif [ "$AREA" -lt 192 ]
+# then
+#     echo "Area is less than 192."
+# else
+#     echo "Enter length and width"
+# fi
+
+# FILE CONDITION
+
+########
+# -d file   True if the file is a directory
+# -e file   True if the file exists (note that this is not particularly portable, thus -f is generally used)
+# -f file   True if the provided string is a file
+# -g file   True if the group id is set on a file
+# -r file   True if the file is readable
+# -s file   True if the file has a non-zero size
+# -u    True if the user id is set on a file
+# -w    True if the file is writable
+# -x    True if the file is an executable
+########
+
+# TESTING IF FILE EXISTS OR NOT
+# echo "Let's check if a file exists or not.
+# "
+
+# read -p "Enter a file name: " FILE
+
+# if [ -e "$FILE" ]
+# then
+#     echo "File does exists"
+# else 
+#     echo "File doesn't exists"
+# fi
+
+# CASE STATEMENT | JUST LIKE SWITCH 
+# Format for case
+# Every letter is placed into square brackets 
+# case "expresion" in
+#   condition )            
+
+
+# # put ;; in the end of the case
+
+# # example CASE STATEMENT
+
+# read -p "are you over 18? " AGE
+
+# case "$AGE" in
+#     [yY] | [yY][eE][sS])
+#     echo "You can have a beer, you're old enough"
+#     ;;
+#     [nN] | [nN][oO])
+#     echo "You can't have a beer you're not old enough"
+#     ;;
+#     *)
+#     echo "Invalid Age"
+#     ;;
+# esac
+
+# FOR LOOP
